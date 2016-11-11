19a79da85f84227465c684a95cb24ef61d32b73c
Mário Monteiro <mail@koakh.com>
2016-11-11 08:29:47 +0000

    ...

---
+++ src/ServerSide/Api/Migrations/20161110191058_MyFirstMigration.Designer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Api.Data;
+
+namespace Api.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20161110191058_MyFirstMigration")]
+    partial class MyFirstMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-preview1-22509")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Api.Models.Movie.Actor", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Age");
+
+                    b.Property<DateTime>("BornDate");
+
+                    b.Property<string>("FirstName")
+                        .HasMaxLength(60);
+
+                    b.Property<string>("LastName")
+                        .HasMaxLength(60);
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Actor");
+                });
+
+            modelBuilder.Entity("Api.Models.Movie.Movie", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("ActorID");
+
+                    b.Property<string>("Genre")
+                        .IsRequired()
+                        .HasMaxLength(30);
+
+                    b.Property<decimal>("Price");
+
+                    b.Property<string>("Rating")
+                        .IsRequired()
+                        .HasMaxLength(5);
+
+                    b.Property<DateTime>("ReleaseDate");
+
+                    b.Property<string>("Title")
+                        .HasMaxLength(60);
+
+                    b.HasKey("ID");
+
+                    b.HasIndex("ActorID");
+
+                    b.ToTable("Movie");
+                });
+
+            modelBuilder.Entity("Api.Models.Movie.Movie", b =>
+                {
+                    b.HasOne("Api.Models.Movie.Actor")
+                        .WithMany("ActInMovies")
+                        .HasForeignKey("ActorID");
+                });
+        }
+    }
+}
---
+++ src/ServerSide/Api/Migrations/20161110191058_MyFirstMigration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Api.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actor",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Age = table.Column<int>(nullable: false),
+                    BornDate = table.Column<DateTime>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 60, nullable: true),
+                    LastName = table.Column<string>(maxLength: 60, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actor", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ActorID = table.Column<int>(nullable: true),
+                    Genre = table.Column<string>(maxLength: 30, nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    Rating = table.Column<string>(maxLength: 5, nullable: false),
+                    ReleaseDate = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(maxLength: 60, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Movie_Actor_ActorID",
+                        column: x => x.ActorID,
+                        principalTable: "Actor",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movie_ActorID",
+                table: "Movie",
+                column: "ActorID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+
+            migrationBuilder.DropTable(
+                name: "Actor");
+        }
+    }
+}
